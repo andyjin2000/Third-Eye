70acaff98063180e45f6f46e1206cbb0b8140b88
Linda He <lindahe@Lindas-MacBook-Air-2.local>
2024-01-13 17:27:09 -0500

    combined yolo and facial

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import math
 import face_recognition
 import cv2
 import numpy as np
+from ultralytics import YOLO
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
- 
+
+
 # Initialize Firebase
 cred = credentials.Certificate('service_accnt_creds.json')
 firebase_admin.initialize_app(cred)
@@ -25,7 +28,7 @@ except Exception as e:
 
 is_facial = doc_ref.get().to_dict()["facial"] == True
 
-def facial_recognition(): 
+def call_recognition(): 
     # Get a reference to webcam #0 (the default one)
     video_capture = cv2.VideoCapture('example2.mp4')
 
@@ -43,7 +46,7 @@ def facial_recognition():
     andy_image = face_recognition.load_image_file("andy.jpg")
     andy_face_encoding = face_recognition.face_encodings(andy_image)[0]
 
-    nikhil_image = face_recognition.load_image_file("nikhil.jpg")
+    nikhil_image = face_recognition.load_image_file("nikhil.png")
     nikhil_face_encoding = face_recognition.face_encodings(nikhil_image)[0]
 
     # Create arrays of known face encodings and their names
@@ -67,6 +70,18 @@ def facial_recognition():
     face_encodings = []
     face_names = []
     process_this_frame = True
+    classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+                "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+                "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+                "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+                "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+                "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+                "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+                "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+                "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+                "teddy bear", "hair drier", "toothbrush"
+                ]
+    model = YOLO("yolo-Weights/yolov8n.pt")
 
     while True:
         # Grab a single frame of video
@@ -76,36 +91,69 @@ def facial_recognition():
 
         # Only process every other frame of video to save time
         if process_this_frame:
-            # Resize frame of video to 1/4 size for faster face recognition processing
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-            # rgb_small_frame = small_frame[:, :, ::-1]
-            rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
-            # rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
-            
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            # if not is_facial:
+            if is_facial:
+                results = model(frame, stream=True)
+                for r in results:
+                    boxes = r.boxes
+                    for box in boxes:
+                        # bounding box
+                        x1, y1, x2, y2 = box.xyxy[0]
+                        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
+                        # put box in cam
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-                # # If a match was found in known_face_encodings, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
+                        # confidence
+                        confidence = math.ceil((box.conf[0]*100))/100
+                        print("Confidence --->",confidence)
 
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
+                        # class name
+                        cls = int(box.cls[0])
+                        print("Class name -->", classNames[cls])
 
-                face_names.append(name)
+                        # object details
+                        org = [x1, y1]
+                        font = cv2.FONT_HERSHEY_SIMPLEX
+                        fontScale = 1
+                        color = (255, 0, 0)
+                        thickness = 2
+
+                        cv2.putText(frame, classNames[cls], org, font, fontScale, color, thickness)
+
+            else:
+                # Resize frame of video to 1/4 size for faster face recognition processing
+                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                # rgb_small_frame = small_frame[:, :, ::-1]
+                rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
+                # rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+                
+                # Find all the faces and face encodings in the current frame of video
+                face_locations = face_recognition.face_locations(rgb_small_frame)
+                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+                face_names = []
+                for face_encoding in face_encodings:
+                    # See if the face is a match for the known face(s)
+                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                    name = "Unknown"
+
+                    # # If a match was found in known_face_encodings, just use the first one.
+                    # if True in matches:
+                    #     first_match_index = matches.index(True)
+                    #     name = known_face_names[first_match_index]
+
+                    # Or instead, use the known face with the smallest distance to the new face
+                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = known_face_names[best_match_index]
+
+                    face_names.append(name)
+
 
         process_this_frame = not process_this_frame
 
@@ -139,7 +187,7 @@ def facial_recognition():
     return name
 
 if is_facial: 
-    name = facial_recognition()
+    name = call_recognition()
 else: 
     # object_detection()
     pass
